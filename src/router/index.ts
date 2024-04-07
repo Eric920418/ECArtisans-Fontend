36ec5a1cac4bfc73efa5c3b64a4f940301ac3075
Eric <26416387.re@gmail.com>
2024-04-07 19:40:35 +0800

    bootstrap and scss fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import index from '../views/PageIndex.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: index
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/randomUser',
-      name: 'randomUser',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RandomUser.vue')
-    }
+
   ]
 })
 
---
+++ src/views/PageIndex.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>這是首頁</h1>
+  <button class="btn btn-primary">132</button>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+
+</style>
