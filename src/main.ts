8c53eb0b70fde915d2a809791d97d3456ad30c5c
陳昱彣 <yuwen@chenyuwendeMacBook-Air.local>
2024-04-07 15:47:11 +0800

    import sass & motify

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 
 const app = createApp(App)
 
