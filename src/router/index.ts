124498b5fc6cf29baf83f03d468ddb96544fec87
stu050418 <124168753+stu050418@users.noreply.github.com>
2024-04-22 22:52:36 +0800

    新增2個頁面、修改eslintrc
    eslintrc補:
    "rules": {
    	"eqeqeq": "error",
    	"no-var": "error",

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,13 @@ import index from '../views/PageIndex.vue'
 
 import eric from '../router/eric.ts'
 import eliza from '../router/eliza.ts'
+import amy from '../router/amy.ts'
 
 const routes = [
   {path: '/',name: 'index',component: index},
   ...eric,
-  ...eliza
+  ...eliza,
+  ...amy
 
 ]
 
