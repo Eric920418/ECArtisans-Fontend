a4622d56bd256ede5675b948c014602692e6e0f7
陳昱彣 <yuwen@chenyuwendeMacBook-Air.local>
2024-04-24 18:34:29 +0800

    修改router,去除ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import index from '../views/PageIndex.vue'
 
-import eric from '../router/eric.ts'
-import eliza from '../router/eliza.ts'
+import eric from '../router/eric'
+import eliza from '../router/eliza'
 
 const routes = [
   {path: '/',name: 'index',component: index},
