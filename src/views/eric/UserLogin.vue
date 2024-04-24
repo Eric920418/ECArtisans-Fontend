d8479dbe24b64d43562a9c74661e99eda4a1c6a5
ERIC955 <26416387.re@gmail.com>
2024-04-24 21:02:36 +0800

    登入註冊

---
+++ src/views/eric/UserLogin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container vh-100 d-flex  justify-content-center align-items-center">
+            <shopLogin ></shopLogin>
+    </div>
+
+    
+</template>
+
+<script setup lang="ts">
+import shopLogin from '/src/components/eric/shopLogin.vue'
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
