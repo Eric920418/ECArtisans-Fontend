29c13742267331703bc0d81dfd149000495fd592
ERIC955 <26416387.re@gmail.com>
2024-04-28 00:34:57 +0800

    登入畫面美化

--- src/components/eric/shopLogin.vue
+++ src/components/eric/shopLogin.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="login">
-        <input class="d-flex mx-auto form-control mb-2" type="text" placeholder="郵件" v-model="mail">
-        <input class="d-flex mx-auto form-control mt-2" type="text" placeholder="密碼" v-model="password">
-        <button class="btn btn-primary mt-2 me-2" @click="login">登入</button>
-        <button class="btn btn-primary mt-2"  @click="change">註冊</button>     
+        <div class=" position-relative">
+            <i class="bi bi-envelope-fill position-absolute z-3 fs-5" style="top:5px; right:467px"></i>
+            <input class="d-flex form-control mb-2 position-relative ps-5 w-50" type="text" placeholder="郵件" v-model="mail">
+        </div>
+        <div class=" position-relative">
+            <i class="bi bi-lock-fill position-absolute z-3 fs-5"  style="top:5px; right:467px"></i>
+            <input class="d-flex  form-control mt-2 position-relative ps-5 w-50" type="password" placeholder="密碼" v-model="password" ref="pas">
+            <i v-if="eye==false" class="bi bi-eye-fill position-absolute z-3 fs-5" style="top:5px; right:260px" @click="see" ></i>
+            <i v-else-if="eye==true" class="bi bi-eye-slash-fill position-absolute z-3 fs-5" style="top:5px; right:260px" @click="see" ></i>
+        </div>
+        <div>
+            <button class="btn btn-primary mt-2 w-50" @click="login">登入</button>
+        </div>
+        <div>
+            <div class="d-flex mt-2">
+                <p class="mt-2">還不是會員?</p>
+                <a class="mt-2 mx-1"  @click.prevent="change">註冊</a>     
+                <a class="mt-2 ms-5"  @click.prevent="change">忘記密碼</a>     
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -13,7 +30,8 @@ export default {
     data() {
         return {
             mail:"" ,
-            password:""
+            password:"",
+            eye: false
         }
     },
 
@@ -26,6 +44,10 @@ export default {
         },
         change() {
             this.$emit('sigin');
+        },
+        see(){
+            this.eye = !this.eye;
+            this.$refs.pas.type = this.eye ? 'text' : 'password';
         }
     },
 }
--- src/views/eric/UserLogin.vue
+++ src/views/eric/UserLogin.vue
@@ -1,10 +1,22 @@
 <template>
 <div>
-    <div v-if="state === 'login'" class="container vh-100 d-flex justify-content-center align-items-center">
-    <shopLogin  @Sigin='handleClick'></shopLogin>
+    <div v-if="state === 'login'" class="container  d-flex justify-content-center align-items-center px-0" style="margin-top: 200px;">
+        <div class="row">
+            <div class="col-8 p-0">
+                <div class=" position-relative">
+                    <h1 class="z-3 position-absolute display-3 fw-bold" style="bottom:350px; left:-50px">ECArtisans</h1>
+                    <h3 class="z-3 position-absolute  fw-bold" style="bottom:300px; left:-50px">探索無限購物樂趣</h3>
+                    <div class="box"></div>
+                </div>
+            </div>
+            <div class="col-4 my-auto">
+                <div class=" fs-3 fw-bold">登入</div>
+                <shopLogin class="mt-3"  @Sigin='handleClick'></shopLogin>
+            </div>
+        </div>
     </div>
     <div v-else-if="state === 'sigin'" class="container vh-100 d-flex justify-content-center align-items-center">
-    <shopSigup></shopSigup>
+    <shopSigup @goback="handleClick2"></shopSigup>
     </div>
 </div>
 </template>
@@ -13,11 +25,10 @@
 <script>
 import shopLogin from '/src/components/eric/shopLogin.vue'
 import shopSigup from '/src/components/eric/shopSigup.vue'
-
 export default {
     components: {
         shopLogin,
-        shopSigup
+        shopSigup,
     },
     data() {
         return {
@@ -28,6 +39,9 @@ export default {
     methods: {
         handleClick(){
             this.state = 'sigin'
+        },
+        handleClick2(){
+            this.state = 'login'
         }
     }
 
@@ -36,5 +50,11 @@ export default {
 
 
 <style scoped>
+.box{
+    width: 600px;
+    height: 400px;
+    background-color: #d1c4c4;
+
+}
 
 </style>
