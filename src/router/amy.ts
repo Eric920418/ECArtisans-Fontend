00f17bf0c05f78bc0355f5372274126d2a41c036
stu050418 <124168753+stu050418@users.noreply.github.com>
2024-04-30 20:01:55 +0800

    新增chart套件，修改CSS與部分頁面
    新增 chart.js 套件
    新增 5-2-8 商家總覽
    修改：
    4-2-2 會員中心 - 調整資料驗證
    5-2-2 商家資訊 - 調整資料驗證

--- src/router/amy.ts
+++ src/router/amy.ts
@@ -1,7 +1,9 @@
-import seller from "../views/amy/SellerArea.vue"
+import ShopHome from "../views/amy/ShopHome.vue"
+import ShopSeller from "../views/amy/ShopSeller.vue"
 import user from "../views/amy/UserArea.vue"
 
 export default [
-    {path: '/seller', name: 'seller', component: seller},
+    {path: '/ShopHome', name: 'ShopHome', component: ShopHome},
+    {path: '/ShopSeller', name: 'ShopSeller', component: ShopSeller},
     {path: '/user', name: 'user', component: user},
 ]
